2bd06c2db73464022cdd9d7206f43abfce1a927b
Le Zhang <le10@michaels.com>
2020-10-23 13:19:32 -0500

    change results function in ap file

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
-import recommendation
+# import recommendation
+from recommender.modeling.prediction import results
 
 app = Flask(__name__)
 CORS(app) 
         
 @app.route('/movie', methods=['GET'])
-def recommend_movies():
-        res = recommendation.results(request.args.get('title'))
-        return jsonify(res)
-
-# from recommender.modeling.prediction import results
-#
 # def recommend_movies():
-#         res = results(request.args.get('title'))
+#         res = recommendation.results(request.args.get('title'))
 #         return jsonify(res)
+
+def recommend_movies():
+        res = results(request.args.get('title'))
+        return jsonify(res)
 
 if __name__=='__main__':
         app.run(port = 5000, debug = True)
